dc203327cfe6ddf634b95b7900c1e48bd917bece
chenw <chenwei@zzdit.com>
2019-06-12 16:39:27 +0800

    feat:搜索框

---
+++ client/src/shared/search/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <input type="text" @keydown="onKeyDown" v-model="inputValue">
+</template>
+
+<script>
+    export default {
+        name: 'shared-search',
+
+        props: {},
+        data() {
+            return {
+                inputValue: ''
+            }
+        },
+        methods: {
+            onKeyDown(e) {
+                if (e.keyCode == 13) {
+                    this.$emit('keydown', this.inputValue)
+                }
+            }
+        }
+
+    }
+</script>
+
+<style lang="scss">
+
+</style>
